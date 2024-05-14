bd1a4e9e52449f5c46127d750db7185a4a7b9634
Diego Whiskey <d.calcar@hotmail.com>
2024-05-13 19:40:03 -0500

    [MODIFIED] CSS Sections and add contact form

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,1 @@
-import "../node_modules/reveal.js/dist/reset.css";
-import "../node_modules/reveal.js/dist/reveal.css";
-import "../node_modules/reveal.js/dist/theme/black.css";
-
-/**
- * Theme used for syntax highlighted code 
- **/
-import "../node_modules/reveal.js/plugin/highlight/monokai.css";
-
-import Reveal from 'reveal.js';
-Reveal.initialize();
-
-
+/* window.alert("Ten la Bienvenida a la página web de Serautos FT\n\nAdvertencia: Esta página web esta en construcción. Así que perdona las molestias.") */
